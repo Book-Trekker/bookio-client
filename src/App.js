caec8f30103210a957fe255bad929743c7fda72e
nazmul162001 <nazmulprime162001@gmail.com>
2022-10-10 22:16:26 +0600

    setUp material tailwind component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Button } from "@material-tailwind/react";
+
 function App() {
   return (
     <div className="">
-      <h2 className="text-red-300">this is header</h2>
-      <h2>commit from development</h2>
-      <h2>commit from nazmul</h2>
+      <Button className="m-5">
+        Click me please!
+      </Button>
     </div>
   );
 }
