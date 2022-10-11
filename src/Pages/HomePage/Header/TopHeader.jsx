6fed18bb2636b8cfa575cbe7e87c9a312adf9a68
nazmul162001 <nazmulprime162001@gmail.com>
2022-10-11 23:16:15 +0600

    added top header

--- src/Pages/HomePage/Header/TopHeader.jsx
+++ src/Pages/HomePage/Header/TopHeader.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const TopHeader = () => {
   return (
-    <section className='top_header'>
-      <h2>This is to header</h2>
+    <section className='top_header py-5 px-10 customBorder'>
+      <div className='flex justify-between items-center'>
+        <div className='menu_link flex gap-3'>
+          <li className='list-none text-[12px] font-lato font-bold text-gray hover:text-black'>
+            <Link to='/'>
+              MEMBERSHIP <span className='font-thin opacity-50 ml-2'>|</span>{' '}
+            </Link>{' '}
+          </li>
+          <li className='list-none text-[12px] font-lato font-bold text-gray hover:text-black'>
+            <Link to='/'>
+              COUPONS & DEALS{' '}
+              <span className='font-thin opacity-50 ml-2'>|</span>
+            </Link>{' '}
+          </li>
+          <li className='list-none text-[12px] font-lato font-bold text-gray hover:text-black'>
+            <Link to='/'>BESTSELLERS</Link>{' '}
+          </li>
+        </div>
+        <div className='account_link'>
+          <div className='flex justify-between items-center gap-3'>
+            <li className='list-none text-[12px] font-lato font-bold text-gray hover:text-black'>
+              <Link to='/'>
+                <span className='mx-2'>
+                  <i class='fas fa-user'></i>
+                </span>{' '}
+                MY ACCOUNT <span className='font-thin opacity-50 ml-2'>|</span>
+              </Link>{' '}
+            </li>
+            <li className='list-none text-[12px] font-lato font-bold text-gray hover:text-black'>
+              <Link to='/'>
+                <span className='mx-2'>
+                  <i class='far fa-heart'></i>
+                </span>{' '}
+                WISHLIST
+              </Link>{' '}
+            </li>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
