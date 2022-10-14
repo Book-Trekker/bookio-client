d89dd01b23268a6e3e8b77576cb213a3ea04fb70
nazmul162001 <nazmulprime162001@gmail.com>
2022-10-15 00:27:37 +0600

    completed hero section

--- src/Pages/HomePage/Hero/Hero.jsx
+++ src/Pages/HomePage/Hero/Hero.jsx
@@ -17,9 +17,9 @@ const Hero = () => {
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
-    // pauseOnHover: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
     ltr: true,
   }
 
@@ -57,12 +57,11 @@ const Hero = () => {
           </div>
           <div className='content absolute'>
             <h1 className='animate__animated animate__fadeInDown animate__delay-2s text-3xl md:text-5xl lg:text-7xl text-primary text-center font-libre font-bold'>
-              Biggest{' '}
-              <span className='text-secondary underline'>Bookstore</span> <br />{' '}
-              in Europe
+              Support <span className='text-secondary underline'>Local</span>{' '}
+              <br /> Bookstores
             </h1>
             <p className='py-4 text-gray font-medium text-center text-xl'>
-              We deliver books all over the world 10,000 + Books in stock
+              $15,343,695.75 raised for local bookstores.
             </p>
             <div className='btn_ text-center'>
               <Button className='bg-secondary rounded-none hover:bg-primary'>
@@ -81,12 +80,11 @@ const Hero = () => {
           </div>
           <div className='content absolute'>
             <h1 className='animate__animated animate__fadeInDown animate__delay-2s text-3xl md:text-5xl lg:text-7xl text-primary text-center font-libre font-bold'>
-              Biggest{' '}
-              <span className='text-secondary underline'>Bookstore</span> <br />{' '}
-              in Europe
+              Bestselling <br />
+              <span className='text-secondary underline'>Fiction</span> Books
             </h1>
             <p className='py-4 text-gray font-medium text-center text-xl'>
-              We deliver books all over the world 10,000 + Books in stock
+              Over 120,000 Trustpilot reviews
             </p>
             <div className='btn_ text-center'>
               <Button className='bg-secondary rounded-none hover:bg-primary'>
