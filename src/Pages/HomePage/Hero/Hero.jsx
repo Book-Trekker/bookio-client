e21d4085dbb618ad7bce8e5717d9fc4706cd0cff
nazmul162001 <nazmulprime162001@gmail.com>
2022-10-15 20:20:01 +0600

    fixed slider speed

--- src/Pages/HomePage/Hero/Hero.jsx
+++ src/Pages/HomePage/Hero/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 3500,
     pauseOnHover: false,
     ltr: true,
   }
