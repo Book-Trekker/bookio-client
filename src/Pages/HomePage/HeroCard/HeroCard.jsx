8db31fe470a869b04510facc62df499b17bfac82
nazmul162001 <nazmulprime162001@gmail.com>
2022-10-16 17:33:36 +0600

    added card content

--- src/Pages/HomePage/HeroCard/HeroCard.jsx
+++ src/Pages/HomePage/HeroCard/HeroCard.jsx
@@ -1,14 +1,47 @@
 import React from 'react'
 import './HeroCard.css'
+import { Button } from '@material-tailwind/react'
 
 const HeroCard = () => {
   return (
-    <section className='hero_card my-10 px-5'>
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-8 w-full'>
+    <section className='hero_card my-10 px-3 md:px-5'>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-7 w-full'>
         {/* card 1 */}
-        <div className='card-bg-1 w-full h-[445px] md:h-44 lg:h-72'></div>
-        <div className='card-bg-2 w-full h-[445px] md:h-44 lg:h-72'></div>
-        <div className='card-bg-3 w-full h-[445px] md:h-44 lg:h-72'></div>
+        <div className='card-bg-1 w-full h-[445px] md:h-44 lg:h-72 relative'>
+          <div className='hero_card_content absolute bottom-7 left-7'>
+            <h2 className='text-3xl my-5 font-libre font-bold text-white'>
+              New <br />
+              Release.
+            </h2>
+            <Button className='bg-[#8F2224] text-[9px] md:text-[12px] rounded-none hover:bg-primary'>
+              Shop Now
+            </Button>
+          </div>
+        </div>
+        {/* card-2  */}
+        <div className='card-bg-2 w-full h-[445px] md:h-44 lg:h-72 relative'>
+          <div className='hero_card_content absolute bottom-7 left-7'>
+            <h2 className='text-3xl my-5 font-libre font-bold text-white'>
+              Pre Order <br />
+              Now.
+            </h2>
+            <Button className='bg-[#2C8A6B] text-[9px] md:text-[12px] rounded-none hover:bg-primary'>
+              Shop Now
+            </Button>
+          </div>
+        </div>
+        {/* card- 3  */}
+        <div className='card-bg-3 w-full h-[445px] md:h-44 lg:h-72 relative'>
+          <div className='hero_card_content absolute bottom-7 left-7'>
+            <h2 className='text-3xl my-5 font-libre font-bold text-white'>
+              Top <br />
+              Rated.
+            </h2>
+            <Button className='bg-[#3076D2] text-[9px] md:text-[12px] rounded-none hover:bg-primary'>
+              Shop Now
+            </Button>
+          </div>
+        </div>
       </div>
     </section>
   )
--- src/Pages/MobileMenu/MobileMenu.jsx
+++ src/Pages/MobileMenu/MobileMenu.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const MobileMenu = () => {
   return (
     <section className='mobile_menu fixed bottom-0 w-full block md:hidden'>
-      <div className='menu bg-primary h-14 text-white grid grid-cols-4 items-center justify-evenly'>
+      <div className='menu bg-white h-14 text-black grid grid-cols-4 items-center justify-evenly'>
         <div className='menu_link text-center'>
           <Link to='/'>
             <i class='ri-shopping-basket-line text-2xl'></i>
