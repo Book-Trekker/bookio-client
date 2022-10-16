fc99baefa385a4265aae0ccc81dcb8626f085f55
nazmul162001 <nazmulprime162001@gmail.com>
2022-10-16 18:56:27 +0600

    fixed footer copyright

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='w-full h-40 flex justify-center items-center'>
-      <p className='text-center text-black'>Copyright @nazmul. 2022</p>
+      <p className='text-center text-black'>
+        Copyright <span className='underline cursor-pointer '>©nazmul</span>-{' '}
+        <span className='underline cursor-pointer '>©sohag</span> . 2022
+      </p>
       <div className='pt-16'></div>
     </footer>
   )
