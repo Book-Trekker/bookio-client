e6f9413631346e921d1e1b2788852ab6b25ba26b
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2022-10-19 00:09:36 +0600

    added tredning slider image

--- src/Pages/HomePage/Trending/Trending.jsx
+++ src/Pages/HomePage/Trending/Trending.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './Trending.css'
 
 const Trending = () => {
   return (
-    <section className='trending px-5 my-8 w-full'>
-      <div className='trending_title flex justify-between items-center'>
+    <section className='trending px-5 my-8 w-full overflow-x-hidden'>
+      {/* trending title  */}
+      <div className='trending_title flex justify-between items-center my-4'>
         <h2 className='text-2xl font-bold font-libre'>Trending On Bookio</h2>
         <div className='flex items-center justify-center'>
           <p className='underline cursor-pointer'>View All</p>
@@ -12,6 +14,8 @@ const Trending = () => {
           </span>
         </div>
       </div>
+
+      {/* trending card  */}
     </section>
   )
 }
