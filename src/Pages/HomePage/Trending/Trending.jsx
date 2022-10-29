ff76a709ce6edc8f74ad98bc295fe309246820ec
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2022-10-29 23:21:30 +0600

    complete trending section

--- src/Pages/HomePage/Trending/Trending.jsx
+++ src/Pages/HomePage/Trending/Trending.jsx
@@ -1,24 +1,14 @@
-import React, { useRef, useState } from 'react'
+import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-import 'swiper/css/pagination'
-import { Pagination } from 'swiper'
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation, Autoplay } from 'swiper'
 import { BsMinecartLoaded } from 'react-icons/bs'
 import { AiOutlineEye } from 'react-icons/ai'
 import './Trending.css'
 
 const Trending = () => {
-  const [hovered, setHovered] = useState(false)
-  const [isHovering, setIsHovering] = useState(false)
-
-  const handleMouseOver = () => {
-    setIsHovering(true)
-  }
-
-  const handleMouseOut = () => {
-    setIsHovering(false)
-  }
-
   return (
     <section className='trending px-5 my-8 w-full overflow-x-hidden'>
       {/* trending title  */}
@@ -36,9 +26,16 @@ const Trending = () => {
       <Swiper
         slidesPerView={5}
         spaceBetween={10}
+        loop={true}
+        freeMode={true}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
         pagination={{
           clickable: true,
         }}
+        navigation={true}
         breakpoints={{
           300: {
             slidesPerView: 1,
@@ -57,42 +54,321 @@ const Trending = () => {
             spaceBetween: 10,
           },
         }}
-        modules={[Pagination]}
+        modules={[Pagination, Navigation, Autoplay]}
         className='mySwiper'
       >
         <SwiperSlide>
-          <div
-            onMouseOver={handleMouseOver}
-            onMouseOut={handleMouseOut}
-            className={
-              isHovering
-                ? 'trending-1 trend-bg-1_1 w-full relative'
-                : 'trending-1 trend-bg-1 w-full relative'
-            }
-          >
-            <div className='trending_content flex justify-between m-3'>
-              <p className='text-sm'>
-                <span className='px-2 bg-yellow text-white py-[1px]'>Hot</span>
+          <div className='w-full change-bg-1 cursor-pointer'>
+            {/* cart  */}
+            <div className='trending-1 trend-bg-1 w-full relative'>
+              <div className='trending_content flex justify-between p-2'>
+                <p className='text-sm'>
+                  <span className='px-2 bg-yellow text-white py-[1px]'>
+                    Hot
+                  </span>
+                </p>
+                <p className=' bg-white text-black rounded-full'>
+                  <span>
+                    <i class='far fa-heart p-2 cursor-pointer hover:bg-primary hover:text-white bg-white text-black rounded-full'></i>
+                  </span>
+                </p>
+              </div>
+              <div className='trending-cart-1 w-full absolute bottom-0'>
+                <div className='action flex justify-between items-center w-full'>
+                  <button className='py-3 bg-black text-white w-full custom-border relative hover:bg-primary flex justify-center items-center'>
+                    <span>
+                      <BsMinecartLoaded className='text-2xl' />
+                    </span>
+                  </button>
+                  <button className='py-3 bg-black text-white w-full hover:bg-primary  flex justify-center items-center'>
+                    <span>
+                      <AiOutlineEye className='text-2xl' />
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* cart details  */}
+            <div className='details'>
+              <p className='author text-sm text-gray mt-2 font-bold'>
+                By: <span className='font-normal'>Alec Hansen</span>
               </p>
-              <p className=' bg-white text-black rounded-full'>
-                <span>
-                  <i class='far fa-heart p-2 cursor-pointer hover:bg-primary hover:text-white bg-white text-black rounded-full'></i>
-                </span>
+              <p className='book-name text-lg font-libre font-medium text-primary'>
+                The Art of Fashion
+              </p>
+              <p className='price font-libre font-bold text-secondary'>
+                $80.00 - $200.00
               </p>
             </div>
-            <div className='trending-cart-1 w-full absolute bottom-0'>
-              <div className='action flex justify-between items-center w-full'>
-                <button className='py-3 bg-black text-white w-full custom-border relative hover:bg-primary flex justify-center items-center'>
-                  <span>
-                    <BsMinecartLoaded className='text-2xl' />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='w-full change-bg-2 cursor-pointer'>
+            {/* cart  */}
+            <div className='trending-2 trend-bg-2 w-full relative'>
+              <div className='trending_content flex justify-between p-2'>
+                <p className='text-sm'>
+                  <span className='px-2 bg-yellow text-white py-[1px]'>
+                    Hot
                   </span>
-                </button>
-                <button className='py-3 bg-black text-white w-full hover:bg-primary  flex justify-center items-center'>
+                </p>
+                <p className=' bg-white text-black rounded-full'>
                   <span>
-                    <AiOutlineEye className='text-2xl' />
+                    <i class='far fa-heart p-2 cursor-pointer hover:bg-primary hover:text-white bg-white text-black rounded-full'></i>
                   </span>
-                </button>
+                </p>
               </div>
+              <div className='trending-cart-2 w-full absolute bottom-0'>
+                <div className='action flex justify-between items-center w-full'>
+                  <button className='py-3 bg-black text-white w-full custom-border relative hover:bg-primary flex justify-center items-center'>
+                    <span>
+                      <BsMinecartLoaded className='text-2xl' />
+                    </span>
+                  </button>
+                  <button className='py-3 bg-black text-white w-full hover:bg-primary  flex justify-center items-center'>
+                    <span>
+                      <AiOutlineEye className='text-2xl' />
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* cart details  */}
+            <div className='details'>
+              <p className='author text-sm text-gray mt-2 font-bold'>
+                By: <span className='font-normal'>Alec Hansen</span>
+              </p>
+              <p className='book-name text-lg font-libre font-medium text-primary'>
+                The Art of Fashion
+              </p>
+              <p className='price font-libre font-bold text-secondary'>
+                $80.00 - $200.00
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='w-full change-bg-3 cursor-pointer'>
+            {/* cart  */}
+            <div className='trending-3 trend-bg-3 w-full relative'>
+              <div className='trending_content flex justify-between p-2'>
+                <p className='text-sm'>
+                  <span className='px-2 bg-yellow text-white py-[1px]'>
+                    Hot
+                  </span>
+                </p>
+                <p className=' bg-white text-black rounded-full'>
+                  <span>
+                    <i class='far fa-heart p-2 cursor-pointer hover:bg-primary hover:text-white bg-white text-black rounded-full'></i>
+                  </span>
+                </p>
+              </div>
+              <div className='trending-cart-3 w-full absolute bottom-0'>
+                <div className='action flex justify-between items-center w-full'>
+                  <button className='py-3 bg-black text-white w-full custom-border relative hover:bg-primary flex justify-center items-center'>
+                    <span>
+                      <BsMinecartLoaded className='text-2xl' />
+                    </span>
+                  </button>
+                  <button className='py-3 bg-black text-white w-full hover:bg-primary  flex justify-center items-center'>
+                    <span>
+                      <AiOutlineEye className='text-2xl' />
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* cart details  */}
+            <div className='details'>
+              <p className='author text-sm text-gray mt-2 font-bold'>
+                By: <span className='font-normal'>Alec Hansen</span>
+              </p>
+              <p className='book-name text-lg font-libre font-medium text-primary'>
+                The Art of Fashion
+              </p>
+              <p className='price font-libre font-bold text-secondary'>
+                $80.00 - $200.00
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='w-full change-bg-4 cursor-pointer'>
+            {/* cart  */}
+            <div className='trending-4 trend-bg-4 w-full relative'>
+              <div className='trending_content flex justify-between p-2'>
+                <p className='text-sm'>
+                  <span className='px-2 bg-yellow text-white py-[1px]'>
+                    Hot
+                  </span>
+                </p>
+                <p className=' bg-white text-black rounded-full'>
+                  <span>
+                    <i class='far fa-heart p-2 cursor-pointer hover:bg-primary hover:text-white bg-white text-black rounded-full'></i>
+                  </span>
+                </p>
+              </div>
+              <div className='trending-cart-4 w-full absolute bottom-0'>
+                <div className='action flex justify-between items-center w-full'>
+                  <button className='py-3 bg-black text-white w-full custom-border relative hover:bg-primary flex justify-center items-center'>
+                    <span>
+                      <BsMinecartLoaded className='text-2xl' />
+                    </span>
+                  </button>
+                  <button className='py-3 bg-black text-white w-full hover:bg-primary  flex justify-center items-center'>
+                    <span>
+                      <AiOutlineEye className='text-2xl' />
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* cart details  */}
+            <div className='details'>
+              <p className='author text-sm text-gray mt-2 font-bold'>
+                By: <span className='font-normal'>Alec Hansen</span>
+              </p>
+              <p className='book-name text-lg font-libre font-medium text-primary'>
+                The Art of Fashion
+              </p>
+              <p className='price font-libre font-bold text-secondary'>
+                $80.00 - $200.00
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='w-full change-bg-5 cursor-pointer'>
+            {/* cart  */}
+            <div className='trending-5 trend-bg-5 w-full relative'>
+              <div className='trending_content flex justify-between p-2'>
+                <p className='text-sm'>
+                  <span className='px-2 bg-yellow text-white py-[1px]'>
+                    Hot
+                  </span>
+                </p>
+                <p className=' bg-white text-black rounded-full'>
+                  <span>
+                    <i class='far fa-heart p-2 cursor-pointer hover:bg-primary hover:text-white bg-white text-black rounded-full'></i>
+                  </span>
+                </p>
+              </div>
+              <div className='trending-cart-5 w-full absolute bottom-0'>
+                <div className='action flex justify-between items-center w-full'>
+                  <button className='py-3 bg-black text-white w-full custom-border relative hover:bg-primary flex justify-center items-center'>
+                    <span>
+                      <BsMinecartLoaded className='text-2xl' />
+                    </span>
+                  </button>
+                  <button className='py-3 bg-black text-white w-full hover:bg-primary  flex justify-center items-center'>
+                    <span>
+                      <AiOutlineEye className='text-2xl' />
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* cart details  */}
+            <div className='details'>
+              <p className='author text-sm text-gray mt-2 font-bold'>
+                By: <span className='font-normal'>Alec Hansen</span>
+              </p>
+              <p className='book-name text-lg font-libre font-medium text-primary'>
+                The Art of Fashion
+              </p>
+              <p className='price font-libre font-bold text-secondary'>
+                $80.00 - $200.00
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='w-full change-bg-6 cursor-pointer'>
+            {/* cart  */}
+            <div className='trending-6 trend-bg-6 w-full relative'>
+              <div className='trending_content flex justify-between p-2'>
+                <p className='text-sm'>
+                  <span className='px-2 bg-yellow text-white py-[1px]'>
+                    Hot
+                  </span>
+                </p>
+                <p className=' bg-white text-black rounded-full'>
+                  <span>
+                    <i class='far fa-heart p-2 cursor-pointer hover:bg-primary hover:text-white bg-white text-black rounded-full'></i>
+                  </span>
+                </p>
+              </div>
+              <div className='trending-cart-6 w-full absolute bottom-0'>
+                <div className='action flex justify-between items-center w-full'>
+                  <button className='py-3 bg-black text-white w-full custom-border relative hover:bg-primary flex justify-center items-center'>
+                    <span>
+                      <BsMinecartLoaded className='text-2xl' />
+                    </span>
+                  </button>
+                  <button className='py-3 bg-black text-white w-full hover:bg-primary  flex justify-center items-center'>
+                    <span>
+                      <AiOutlineEye className='text-2xl' />
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* cart details  */}
+            <div className='details'>
+              <p className='author text-sm text-gray mt-2 font-bold'>
+                By: <span className='font-normal'>Alec Hansen</span>
+              </p>
+              <p className='book-name text-lg font-libre font-medium text-primary'>
+                The Art of Fashion
+              </p>
+              <p className='price font-libre font-bold text-secondary'>
+                $80.00 - $200.00
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='w-full change-bg-7 cursor-pointer'>
+            {/* cart  */}
+            <div className='trending-7 trend-bg-7 w-full relative'>
+              <div className='trending_content flex justify-between p-2'>
+                <p className='text-sm'>
+                  <span className='px-2 bg-yellow text-white py-[1px]'>
+                    Hot
+                  </span>
+                </p>
+                <p className=' bg-white text-black rounded-full'>
+                  <span>
+                    <i class='far fa-heart p-2 cursor-pointer hover:bg-primary hover:text-white bg-white text-black rounded-full'></i>
+                  </span>
+                </p>
+              </div>
+              <div className='trending-cart-7 w-full absolute bottom-0'>
+                <div className='action flex justify-between items-center w-full'>
+                  <button className='py-3 bg-black text-white w-full custom-border relative hover:bg-primary flex justify-center items-center'>
+                    <span>
+                      <BsMinecartLoaded className='text-2xl' />
+                    </span>
+                  </button>
+                  <button className='py-3 bg-black text-white w-full hover:bg-primary  flex justify-center items-center'>
+                    <span>
+                      <AiOutlineEye className='text-2xl' />
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* cart details  */}
+            <div className='details'>
+              <p className='author text-sm text-gray mt-2 font-bold'>
+                By: <span className='font-normal'>Alec Hansen</span>
+              </p>
+              <p className='book-name text-lg font-libre font-medium text-primary'>
+                The Art of Fashion
+              </p>
+              <p className='price font-libre font-bold text-secondary'>
+                $80.00 - $200.00
+              </p>
             </div>
           </div>
         </SwiperSlide>
