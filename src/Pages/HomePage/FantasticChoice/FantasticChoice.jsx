6d1d1d4293e429f6f5a0567cae565e597deb7138
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2022-12-23 16:39:47 +0600

    completed fantastic section

--- src/Pages/HomePage/FantasticChoice/FantasticChoice.jsx
+++ src/Pages/HomePage/FantasticChoice/FantasticChoice.jsx
@@ -3,17 +3,17 @@ import './FantasticChoice.css'
 
 const FantasticChoice = () => {
   return (
-    <section className='w-full my-16'>
-      <div className='choice_main w-[85%] h-[600px] flex items-center relative'>
+    <section className='w-full my-44'>
+      <div className='choice_main w-full md:w-[85%] h-[600px] flex items-center relative'>
         <div className='pl-10 text-white'>
           <p className='font-bold font-lato text-lg'>
             Welcome to the Wonderful World of Books
           </p>
-          <h2 className='text-5xl font-libre font-bold my-5'>
+          <h2 className='text-3xl md:text-5xl font-libre font-bold my-5'>
             Cheap Books, <br /> Fantastic Choice{' '}
           </h2>
           <hr className='w-[60px] border-b-2' />
-          <p className='w-3/5 pr-3 my-8'>
+          <p className='w-full md:w-3/5 pr-3 my-8'>
             If you’re looking for great value second hand books, then World of
             Books is the place for you. As you may already know, we aren’t like
             other online book stores. For starters, we don’t believe that books
@@ -22,6 +22,8 @@ const FantasticChoice = () => {
             used books find new homes every year.{' '}
           </p>
           <button className='border-2 px-8 py-2 font-bold font-lato hover:bg-black hover:border-transparent transition-all duration-[.2s] '>Learn more</button>
+        </div>
+        <div className='h-[700px] w-1/2 hidden md:block side-bg absolute right-[-200px] bottom-[-200px]'>
         </div>
       </div>
     </section>
