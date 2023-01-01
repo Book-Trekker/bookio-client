1e08e550d68817035990ac43d103d74d01b22827
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2023-01-01 22:54:31 +0600

    added login form complete

--- src/Pages/Account/Login/Login.jsx
+++ src/Pages/Account/Login/Login.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Login.css'
+import { Checkbox } from '@material-tailwind/react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <section className='login h-[300px] border-2 border-[#eaeaea] rounded pt-12'>
+    <section className='login border-2 border-[#eaeaea] rounded pt-12 pb-4'>
       <div className='login_title'>
         <h2 className='custom_border relative flex items-center justify-center text-center text-2xl font-bold text-black font-libre pb-2'>
           <span>
@@ -13,17 +15,40 @@ const Login = () => {
         </h2>
       </div>
       <div className='input_field px-12 py-5'>
-        <div className='input_email flex flex-col'>
+        {/* input email  */}
+        <div className='input_email flex flex-col mb-2'>
           <label htmlFor='email' className='input-label pb-2 text-[#767a79]'>
             Username or email address <span className='text-red-500'>*</span>
           </label>
           <input
-          className='border-2 px-2 py-4 border-[#eaeaea]'
+            className='border-2 px-2 py-4 border-[#eaeaea]'
             type='email'
             autoComplete='off'
             name='email'
             id='email'
           />
+        </div>
+        {/* input password  */}
+        <div className='input_email flex flex-col mb-2'>
+          <label htmlFor='password' className='input-label pb-2 text-[#767a79]'>
+            Password <span className='text-red-500'>*</span>
+          </label>
+          <input
+            className='border-2 px-2 py-4 border-[#eaeaea]'
+            type='password'
+            autoComplete='off'
+            name='password'
+            id='password'
+          />
+        </div>
+        {/* remember me  */}
+        <div className='remember_me flex justify-between items-center'>
+          <Checkbox className='rounded-full' label='Remember Me' />
+          <Link to={""} className='border-b-[1px]'>Lost Your Password?</Link>
+        </div>
+        {/* login button  */}
+        <div className="login_btn">
+            <button className='text-center bg-black w-full text-white py-4 text-sm'>LOGIN</button>
         </div>
       </div>
     </section>
