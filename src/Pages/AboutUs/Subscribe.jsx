1be9086d7169be84d50e2c19de0939080b89b505
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2023-02-18 21:49:27 +0600

    update subscribe link

--- src/Pages/AboutUs/Subscribe.jsx
+++ src/Pages/AboutUs/Subscribe.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import subs from '../../assets/images/about/subs-bg.png'
 import { Input } from '@material-tailwind/react'
+import '../../style/Global.css'
 
 const Subscribe = () => {
   return (
@@ -16,10 +17,12 @@ const Subscribe = () => {
             Straight to your inbox.
           </h3>
           {/* input field  */}
-          <div className='w-1/3 mx-auto text-white flex'>
+          <div className='w-1/3 mx-auto text-white flex sub-btn'>
             <Input className='focus:text-white' variant='your email address' label='your email address' />
             <button className='border-b ml-2'>Subscribe</button>
           </div>
+          <p className='text-white font-lato text-[14px] my-5'>By clicking subscribe, I acknowledge that I have read and agree to Bookio’s</p>
+          <a className='text-white underline' href="http://google.com" target="_blank" rel="noreferrer">Terms of Use and Privacy Policy.</a>
         </div>
       </div>
     </section>
