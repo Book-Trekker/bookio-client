388105b50810363327d7f501bcf6fbc9a930d131
sohag.zayan <gsohag360@gmail.com>
2023-02-19 09:48:19 -0600

    complete shop page with responsive filtering side bar

--- src/Pages/Shop/ProductHeader.jsx
+++ src/Pages/Shop/ProductHeader.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
-import { pageSizes } from "../../utilities/Filtering";
+import { pageSizes, sortFiltering } from "../../utilities/Filtering";
 
-const ProductHeader = () => {
+const ProductHeader = ({ showMobileFiltering, setShowMobileFiltering }) => {
   const [showPerPageMenu, setShowPerPageMenu] = useState(false);
   const [showSortingMenu, setShowSortingMenu] = useState(false);
   const [pageSize, setPageSize] = useState(12);
+  const [sorts, setSorts] = useState(sortFiltering[0]);
 
   const handleSetPageSize = (size) => {
     setPageSize(size);
     setShowPerPageMenu(false);
   };
-  const handleSetSorting = (size) => {
-    setPageSize(size);
+  const handleSetSorting = (sort) => {
+    setSorts(sort);
     setShowSortingMenu(false);
   };
   return (
@@ -19,47 +20,61 @@ const ProductHeader = () => {
       <div>
         <h3 className="text-[22px] font-semibold mb-2">Shop</h3>
         <div className="bg-[#F3F3F3] py-3 px-3 flex justify-between items-center">
-          <div className="inline-flex items-center relative bg-[#fff] px-4 py-1 cursor-pointer">
-            <h3 onClick={() => setShowPerPageMenu((prev) => !prev)}>
-              Show <span className="ml-2"> {pageSize}</span>{" "}
-            </h3>
-            <ul
-              className={
-                showPerPageMenu ? "showPerPageMenu active" : "showPerPageMenu"
-              }
+          <div className="flex items-center">
+            <div
+              onClick={() => setShowMobileFiltering(true)}
+              className="mr-2 lg:hidden block cursor-pointer"
             >
-              {pageSizes.map((size, index) => (
-                <li
-                  onClick={() => handleSetPageSize(size)}
-                  className="px-[15px] py-1 text-[1rem]"
-                >
-                  {size}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div>
+              <i className="ri-sound-module-fill text-[23px]"></i>
+            </div>
             <div className="inline-flex items-center relative bg-[#fff] px-4 py-1 cursor-pointer">
-              <h3
-                className="flex items-center"
-                onClick={() => setShowSortingMenu((prev) => !prev)}
-              >
-                Default Sorting <i className="ri-arrow-down-s-line"></i>
+              <h3 onClick={() => setShowPerPageMenu((prev) => !prev)}>
+                Show <span className="ml-2"> {pageSize}</span>{" "}
               </h3>
               <ul
                 className={
-                  showSortingMenu ? "showPerPageMenu active" : "showPerPageMenu"
+                  showPerPageMenu ? "showPerPageMenu active" : "showPerPageMenu"
                 }
               >
                 {pageSizes.map((size, index) => (
                   <li
-                    onClick={() => handleSetSorting(size)}
+                    onClick={() => handleSetPageSize(size)}
                     className="px-[15px] py-1 text-[1rem]"
                   >
                     {size}
                   </li>
                 ))}
               </ul>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <div className="inline-flex items-center relative bg-[#fff] px-4 py-1 cursor-pointer">
+              <h3
+                className="flex items-center"
+                onClick={() => setShowSortingMenu((prev) => !prev)}
+              >
+                {sorts.name} <i className="ri-arrow-down-s-line"></i>
+              </h3>
+              <ul
+                className={
+                  showSortingMenu ? "showPerPageMenu active" : "showPerPageMenu"
+                }
+              >
+                {sortFiltering.map((sort, index) => (
+                  <li
+                    onClick={() => handleSetSorting(sort)}
+                    className="px-[15px] py-1 text-[1rem]"
+                  >
+                    {sort.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <div className="flex items-center">
+                <i className="ri-grid-fill text-[25px] px-1"></i>
+                <i className="ri-layout-grid-fill text-[24px] px-1"></i>
+              </div>
             </div>
           </div>
         </div>
--- src/Pages/Shop/Products.jsx
+++ src/Pages/Shop/Products.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineEye } from "react-icons/ai";
+import { BsMinecartLoaded } from "react-icons/bs";
+import { shopProductData } from "../../utilities/shopData";
 import Filtering from "./Filtering/Filtering";
+import MobileFiltering from "./Filtering/MobileFiltering";
+import Product from "./Product";
 import ProductHeader from "./ProductHeader";
 
 const Products = () => {
+  const [showMobileFiltering, setShowMobileFiltering] = useState(false);
   return (
     <div>
       <div className="grid grid-cols-12 px-10">
-        <div className="col-span-3">
+        <div className="lg:col-span-3 lg:block hidden">
           <Filtering />
         </div>
-        <div className="col-span-9">
-          <ProductHeader />
+        <div className=" col-span-12 lg:col-span-9">
+          <ProductHeader {...{ showMobileFiltering, setShowMobileFiltering }} />
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 mt-10">
+            {shopProductData.map((p) => (
+              <Product />
+            ))}
+          </div>
         </div>
+      </div>
+      <div className="relative">
+        <MobileFiltering {...{ showMobileFiltering, setShowMobileFiltering }} />
       </div>
     </div>
   );
