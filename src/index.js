6f752dffd2f48d8a2d2ec31e48a0914dd82dd740
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-07-19 14:06:31 +0600

    update latest code

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import App from './App'
 import { ThemeProvider } from '@material-tailwind/react'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { ApiProvider } from '@reduxjs/toolkit/query/react'
+// import { ApiProvider } from '@reduxjs/toolkit/query/react'
 import { store } from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
-    <ApiProvider>
+    {/* <ApiProvider> */}
       <BrowserRouter>
         <ThemeProvider>
           <App />
         </ThemeProvider>
       </BrowserRouter>
-    </ApiProvider>
+    {/* </ApiProvider> */}
   </Provider>
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { apiSlice } from './features/auth/apiSlice'
+import authReducer from './features/auth/authSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 })
