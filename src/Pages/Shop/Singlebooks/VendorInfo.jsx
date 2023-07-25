18f47dcd01c9ce0c60af7486fcbc01037199d03b
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-07-25 10:33:34 +0600

    added vendor information

--- src/Pages/Shop/Singlebooks/VendorInfo.jsx
+++ src/Pages/Shop/Singlebooks/VendorInfo.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const VendorInfo = () => {
     return (
         <div>
-            <h2>Vendor info here</h2>
+            <h2 className='font-libre font-bold text-xl my-5'>VENDOR INFORMATION</h2>
+            <p className='py-2 font-normal text-gray text-sm'>Store Name: <span>Bookio</span></p>
+            <p className='py-2 font-normal text-gray text-sm'>Vendor: <span className='text-black font-normal text-opacity-95'>Bookio</span></p>
+            <p className='py-2 font-normal text-gray text-sm'>Address: <span>Bookio</span></p>
+            <p className='py-2 font-normal text-gray text-sm'><span>5.00 </span> rating from 1 user</p>
         </div>
     );
 };
